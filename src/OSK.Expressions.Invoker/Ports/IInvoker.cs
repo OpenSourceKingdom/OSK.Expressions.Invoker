5d2bd3c504d1b8d0aaa764c735a764ce41cceccb
BlankDev117 <mmmasent@gmail.com>
2026-05-07 18:44:50 -0400

    Allow Getters and update docs (OSK-5)
    Motivation
    ----
    Getter methods do not work
    
    Modifications
    ----
    * Updated FastInvoker to account for getter
    * Updated InvokerFactory to handle creating getter callbacks
    
    Result
    ----
    Invoker now supports getter fields/properties

--- src/OSK.Expressions.Invoker/Ports/IInvoker.cs
+++ src/OSK.Expressions.Invoker/Ports/IInvoker.cs
@@ -2,37 +2,36 @@
 using OSK.Hexagonal.MetaData;
 using System;
 
-namespace OSK.Expressions.Invoker.Ports
+namespace OSK.Expressions.Invoker.Ports;
+
+[HexagonalIntegration(HexagonalIntegrationType.LibraryProvided)]
+public interface IInvoker
 {
-    [HexagonalIntegration(HexagonalIntegrationType.LibraryProvided)]
-    public interface IInvoker
-    {
-        /// <summary>
-        /// The type of invocation this invoker is configured to support
-        /// </summary>
-        InvocationType InvocationType { get; }
+    /// <summary>
+    /// The type of invocation this invoker is configured to support
+    /// </summary>
+    InvocationType InvocationType { get; }
 
-        /// <summary>
-        /// The type of object that should be passed to the invoker
-        /// </summary>
-        Type InvokeTargetType { get; }
+    /// <summary>
+    /// The type of object that should be passed to the invoker
+    /// </summary>
+    Type InvokeTargetType { get; }
 
-        /// <summary>
-        /// The expected parameter types to be provided to the invoker to function
-        /// </summary>
-        Type[] ParameterTypes { get; }
+    /// <summary>
+    /// The expected parameter types to be provided to the invoker to function
+    /// </summary>
+    Type[] ParameterTypes { get; }
 
-        /// <summary>
-        /// The return type of the member info that was used to create this invoker
-        /// </summary>
-        Type ReturnType { get; }
+    /// <summary>
+    /// The return type of the member info that was used to create this invoker
+    /// </summary>
+    Type ReturnType { get; }
 
-        /// <summary>
-        /// Effeciently run the invoker on the targeted object
-        /// </summary>
-        /// <param name="invokeTarget">The object to invoke</param>
-        /// <param name="parameters">The parameters to pass to the invocation</param>
-        /// <returns></returns>
-        object FastInvoke(object invokeTarget, object[] parameters);
-    }
+    /// <summary>
+    /// Effeciently run the invoker on the targeted object
+    /// </summary>
+    /// <param name="invokeTarget">The object to invoke</param>
+    /// <param name="parameters">The parameters to pass to the invocation</param>
+    /// <returns></returns>
+    object FastInvoke(object invokeTarget, object[] parameters);
 }
